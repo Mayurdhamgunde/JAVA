59e26958cc091df760893d6f3786b08bb024df49
Mayur Dhamgunde <mayurdhamgunde@gmail.com>
2024-06-05 14:03:18 +0530

    Initial commit

---
+++ JAVA SPRINGBOOT/Dependency-Injection/tutorial (1)/tutorial/src/main/java/com/codingshuttle/mayurtutorial/tutorial/DevDB.java
@@ -0,0 +1,8 @@
+package com.codingshuttle.mayurtutorial.tutorial;
+
+public class DevDB implements DB{
+
+    public String getData(){
+        return "Dev Data";
+    }
+}
---
+++ JAVA SPRINGBOOT/Dependency-Injection/tutorial (1)/tutorial/src/main/java/com/codingshuttle/mayurtutorial/tutorial/MayurTutApplication.java
@@ -0,0 +1,23 @@
+package com.codingshuttle.mayurtutorial.tutorial;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MayurTutApplication implements CommandLineRunner {
+
+	@Autowired
+	DB db;
+
+	public static void main(String[] args) {
+		SpringApplication.run(MayurTutApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(db.getData());
+	}
+}
